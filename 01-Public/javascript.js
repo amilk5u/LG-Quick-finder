14fa2ee9ea4821d496c4ca22b0c840de4ec9ffb2
minju shin <amilk5u@naver.com>
2022-03-11 01:43:20 +0900

    2022.03.11 commit

--- 01-Public/javascript.js
+++ 01-Public/javascript.js
@@ -1,13 +1,31 @@
-// filter (세탁기)
-const _filter = {
+// filter 
+let _filter = {
   Cat: ["WD", "WM"],
   size: [8, 9, 10, 10.5, 12],
   color: ["white", "graphite", "black steel"],
   intent: ["Price","Features"],
 };
 
+let _filter1 = {
+   Cat: ["WD"],
+   size: [8, 12],
+   color: ["white", "black steel"],
+   intent: ["Price"],
+ };
+ 
+
+/* 
+   1. input 클릭시 input value 를 가져와서 filter에 해당하는 부분에 push 해준다
+   2. 다음스탭도 동일하게 push 해준다
+   3. 비교에서는 value 를 반복 후 제품들과 비교하여 true 일 때 반환한다
+*/
+
+console.log(_filter1.Cat);
+
+
+
 // product
-const 세탁기 = [
+const washingMachine = [
   {
     product: "product01",
     Cat: "WM",
@@ -20,7 +38,7 @@ const 세탁기 = [
     Cat: "WD",
     size: 9,
     color: "black steel",
-	intent: "Price",
+	intent: "Features",
   },
   {
     product: "product03",
@@ -65,27 +83,37 @@ const 세탁기 = [
 	intent: "Features"
   }
 ];
+// console.log(washingMachine);
 
-const $wrap = $("#wrap");
-const test2 = 세탁기.filter((item) => {
-  if (item.Cat === "WD" &&  item.intent === "Features" ) {
-    return (
-      item.size === 9 ||
-      item.color === "back stell" ||
-      item.color === "white"
-    );
-  }
+const $wrap = $("#wrap"),
+      $view = $wrap.find(".view");
+const test2 = washingMachine.map((item) => {
+   if (item.Cat === _filter1.Cat) {
+      return item
+   }
+
+
+
+   
+ /*  if (item.Cat === _filter1.Cat &&  item.intent === _filter1.intent ) {
+  } */
 });
+
+
+
+
 console.log(test2);
 
-// 뿌리기
+/* // 뿌리기
 test2.forEach(function (item, index) {
-  $wrap.append(
+  $view.append(
     `<div class="box"> 
 		<span>${item.product}</span>
-		<span>${item.Cat}</span><span> ${item.size}</span>
+		<span>${item.Cat}</span>
+      <span> ${item.size}</span>
 		<span>${item.color}</span> 
+		<span>${item.intent}</span> 
 	</div>
 	`
   );
-});
+}); */
