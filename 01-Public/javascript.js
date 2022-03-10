c55e3da154116f33d0dc026e59b47cbdda1287b3
minju <amilk5u@naver.com>
2022-03-10 19:43:27 +0900

    second commit

--- 01-Public/javascript.js
+++ 01-Public/javascript.js
@@ -3,7 +3,8 @@ const _filter = {
   Cat: ["WD", "WM"],
   size: [8, 9, 10, 10.5, 12],
   color: ["white", "graphite", "black steel"],
-}
+  intent: ["Price","Features"],
+};
 
 // product
 const 세탁기 = [
@@ -12,48 +13,70 @@ const 세탁기 = [
     Cat: "WM",
     size: 8,
     color: "graphite",
+    intent: "Price",
   },
   {
     product: "product02",
     Cat: "WD",
     size: 9,
     color: "black steel",
+	intent: "Price",
   },
   {
     product: "product03",
     Cat: "WM",
     size: 10,
     color: "white",
+	intent: "Price",
   },
   {
     product: "product04",
     Cat: "WD",
     size: 10.5,
     color: "black steel",
+	intent: "Price",
   },
   {
     product: "product05",
     Cat: "WM",
     size: 12,
     color: "graphite",
+	intent: "Features"
   },
   {
     product: "product06",
     Cat: "WD",
     size: 12,
     color: "white",
+	intent: "Features"
+  },
+  {
+    product: "product07",
+    Cat: "WM",
+    size: 8,
+    color: "black steel",
+	intent: "Price"
+  },
+  {
+    product: "product08",
+    Cat: "WD",
+    size: 9,
+    color: "graphite",
+	intent: "Features"
   }
-]
+];
 
 const $wrap = $("#wrap");
 const test2 = 세탁기.filter((item) => {
-  if (item.Cat === "WD") {
-    return item.size === 10.5 
-	|| item.color === "back stell" 
-	|| item.color === "white"
-  } 
-})
-console.log(test2)
+  if (item.Cat === "WD" &&  item.intent === "Features" ) {
+    return (
+      item.size === 9 ||
+      item.color === "back stell" ||
+      item.color === "white"
+    );
+  }
+});
+console.log(test2);
 
 // 뿌리기
 test2.forEach(function (item, index) {
@@ -64,5 +87,5 @@ test2.forEach(function (item, index) {
 		<span>${item.color}</span> 
 	</div>
 	`
-  )
-})
+  );
+});
