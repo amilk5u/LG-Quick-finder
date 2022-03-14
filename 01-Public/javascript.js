5bc318bd672234c2ff0d45343bb8c5c066c64bfa
ample amilk5u <amilk5u@naver.com>
2022-03-14 15:57:45 +0900

    2022.03.14 commit

--- 01-Public/javascript.js
+++ 01-Public/javascript.js
@@ -1,4 +1,4 @@
-const $wrap = $('#wrap')
+const $wrap = $("#wrap");
 
 // Filter Example
 // Category: ['WD', 'WM'],
@@ -13,183 +13,198 @@ const insertOption = {
   Size: [],
   Color: [],
   Intent: [],
-}
+};
 
 // Product
 const washingMachine = [
   {
-    Product: 'product01',
-    Category: 'WM',
+    Product: "product01",
+    Category: "WM",
     Capacity: 8,
     Size: 500,
-    Color: 'graphite',
-    Intent: 'Price',
+    Color: "graphite",
+    Intent: "Price",
   },
   {
-    Product: 'product02',
-    Category: 'WD',
+    Product: "product02",
+    Category: "WD",
     Capacity: 9,
     Size: 600,
-    Color: 'black steel',
-    Intent: 'Features',
+    Color: "black steel",
+    Intent: "Features",
   },
   {
-    Product: 'product03',
-    Category: 'WM',
+    Product: "product03",
+    Category: "WM",
     Capacity: 10,
     Size: 700,
-    Color: 'white',
-    Intent: 'Price',
+    Color: "white",
+    Intent: "Price",
   },
   {
-    Product: 'product04',
-    Category: 'WD',
+    Product: "product04",
+    Category: "WD",
     Capacity: 10.5,
     Size: 500,
-    Color: 'black steel',
-    Intent: 'Price',
+    Color: "black steel",
+    Intent: "Price",
   },
   {
-    Product: 'product05',
-    Category: 'WM',
+    Product: "product05",
+    Category: "WM",
     Capacity: 12,
     Size: 600,
-    Color: 'graphite',
-    Intent: 'Price',
+    Color: "graphite",
+    Intent: "Price",
   },
   {
-    Product: 'product06',
-    Category: 'WD',
+    Product: "product06",
+    Category: "WD",
     Capacity: 8,
     Size: 700,
-    Color: 'white',
-    Intent: 'Features',
+    Color: "white",
+    Intent: "Features",
   },
   {
-    Product: 'product07',
-    Category: 'WM',
+    Product: "product07",
+    Category: "WM",
     Capacity: 9,
     Size: 600,
-    Color: 'black steel',
-    Intent: 'Features',
+    Color: "black steel",
+    Intent: "Features",
   },
   {
-    Product: 'product08',
-    Category: 'WD',
+    Product: "product08",
+    Category: "WD",
     Capacity: 10.5,
     Size: 500,
-    Color: 'graphite',
-    Intent: 'Features',
+    Color: "graphite",
+    Intent: "Features",
   },
   {
-    Product: 'product09',
-    Category: 'WM',
+    Product: "product09",
+    Category: "WM",
     Capacity: 12,
     Size: 700,
-    Color: 'white',
-    Intent: 'Price',
+    Color: "white",
+    Intent: "Price",
   },
   {
-    Product: 'product10',
-    Category: 'WM',
+    Product: "product10",
+    Category: "WM",
     Capacity: 8,
     Size: 600,
-    Color: 'black steel',
-    Intent: 'Price',
+    Color: "black steel",
+    Intent: "Price",
   },
   {
-    Product: 'product11',
-    Category: 'WD',
+    Product: "product11",
+    Category: "WD",
     Capacity: 10.5,
     Size: 500,
-    Color: 'graphite',
-    Intent: 'Features',
+    Color: "graphite",
+    Intent: "Features",
   },
   {
-    Product: 'product12',
-    Category: 'WM',
+    Product: "product12",
+    Category: "WM",
     Capacity: 12,
     Size: 500,
-    Color: 'graphite',
-    Intent: 'Features',
+    Color: "graphite",
+    Intent: "Features",
   },
   {
-    Product: 'product13',
-    Category: 'WD',
+    Product: "product13",
+    Category: "WD",
     Capacity: 9,
     Size: 700,
-    Color: 'black steel',
-    Intent: 'Features',
+    Color: "black steel",
+    Intent: "Features",
   },
   {
-    Product: 'product14',
-    Category: 'WD',
+    Product: "product14",
+    Category: "WD",
     Capacity: 8,
     Size: 600,
-    Color: 'white',
-    Intent: 'Price',
+    Color: "white",
+    Intent: "Price",
   },
   {
-    Product: 'product15',
-    Category: 'WM',
+    Product: "product15",
+    Category: "WM",
     Capacity: 10,
     Size: 500,
-    Color: 'graphite',
-    Intent: 'Features',
+    Color: "graphite",
+    Intent: "Features",
   },
   {
-    Product: 'product16',
-    Category: 'WM',
+    Product: "product16",
+    Category: "WM",
     Capacity: 9,
     Size: 700,
-    Color: 'white',
-    Intent: 'Features',
+    Color: "white",
+    Intent: "Features",
   },
-]
+];
 // console.log(washingMachine)
 
 function inputCheck() {
-  $("input[name='Category']:checked").each((function () {
-    insertOption.Category.push($(this).val())
-  }))
-  $("input[name='Intent']:checked").each(function () {
-    insertOption.Intent.push($(this).val())
+  $(".option").each(function () {
+    $(this).find("input:checked").each(function () {
+      if ( $(this).attr("name") === "Category" ) {
+        insertOption.Category.push($(this).val())
+      } else if ( $(this).attr("name") === "Intent" ) {
+        insertOption.Intent.push($(this).val())
+      } else if ( $(this).attr("name") === "Capacity" ) {
+        insertOption.Capacity.push($(this).val())
+      } else if ( $(this).attr("name") === "Color" ) {
+        insertOption.Color.push($(this).val())
+      }
+    })
   })
-  $("input[name='Capacity']:checked").each(function () {
-    insertOption.Capacity.push($(this).val())
-  })
-  $("input[name='Color']:checked").each(function () {
-    insertOption.Color.push($(this).val())
-  })
-  newFunction()
-
-   function newFunction() {
-      console.log(insertOption)
-   }
+  console.log(insertOption);
 }
+
 function sortOption(item, index) {
   // Capacity 에 해당하는 것과 Color 에 해당하는 것이 있을 때 그 값을 return
   for (let i = 0; i < insertOption.Capacity.length; i++) {
+    console.log(insertOption.Capacity[i]);
     if (item.Capacity == insertOption.Capacity[i]) {
-      return item
+      return item;
     }
   }
   for (let i = 0; i < insertOption.Color.length; i++) {
     if (item.Color == insertOption.Color[i]) {
-      return item
+      return item;
     }
   }
+
+  // 현재 선택한 옵션의 갯수 만큼 반복해서 나열
+  /*   for (let i = 0; i < insertOption.Capacity.length; i++) {
+    
+  } */
+
+  // Capacity 와 Color 모두 해당하는 것을 return
+  /*  if (
+    item.Capacity == ddd && item.Color === ccc
+  ) {
+    return item;
+  } */
 }
 
 function filterSelect() {
-   // Product 중 같은 value 가 있을 시 그 값을 return
+  inputCheck();
+  // Product 중 같은 value 가 있을 시 그 값을 return
   const washSelectMachine = washingMachine.filter((item, index) => {
-    if (item.Category === insertOption.Category[0] && item.Intent === insertOption.Intent[0]) {
-      return sortOption(item, index)
+    if (
+      item.Category === insertOption.Category[0] &&
+      item.Intent === insertOption.Intent[0]
+    ) {
+      return sortOption(item, index);
     }
-  })
+  });
 
-  // Load 
+  // Load
   washSelectMachine.forEach(function (item) {
     $wrap.append(
       `<div class="box"> 
@@ -200,12 +215,9 @@ function filterSelect() {
           <span>${item.Color}</span> 
           <span>${item.Intent}</span> 
        </div>
-      `,
-    )
-  })
+      `
+    );
+  });
 }
 
-$('.submit_btn').on('click', function () {
-  inputCheck()
-  filterSelect()
-})
+$(".submit_btn").on("click", filterSelect);
